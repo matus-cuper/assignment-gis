3c5ae4527237cdc7a2413859301ca806e65c5bd4
Matus Cuper <matus.cuper@gmail.com>
2017-11-14 16:03:46 +0100

    Test node.js server script for API calls

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-app.listen(port);
 
-console.log('fiit-pdt Project API server started on: ' + port)
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+router.use(function(req, res, next) {
+	console.log('Something is happening.');
+	next();
+});
+
+router.get('/', (req, res) => res.json({ message: 'API state is OK!' }));
+
+router.route('/points')
+	.post(function(req, res) {
+		res.json({ message: 'API state is OK! POST accepted' });
+	})
+	.get(function(req, res) {
+		res.json({ message: 'API state is OK! GET accepted' });
+	});
+
+
+app.use('/api', router);
+app.listen(port, () => console.log('Example app listening on port 3000 and running on http://127.0.0.1:3000'));
