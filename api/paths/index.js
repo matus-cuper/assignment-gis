245af68fde4b18153cafebb51633a36ebbc71c3a
Matus Cuper <matus.cuper@gmail.com>
2017-11-17 10:38:45 +0100

    Add logging query parameters

--- api/paths/index.js
+++ api/paths/index.js
@@ -7,16 +7,14 @@ var sqlQuery = fs.readFileSync('queries/amen-test.sql').toString();
 
 router.get('/', function(req, res, next) {
   console.log('Accessing /api/paths');
-  const pool = req.app.get('pool')
-
   console.log(sqlQuery);
+  const pool = req.app.get('pool');
 
   pool.connect((err, client, done) => {
     if (err) throw err;
     client.query(sqlQuery, ['guest_house'], (err, result) => {
       done();
       console.log('Returned ' + result.rowCount + ' rows');
-      console.log(sqlQuery);
 
       var i;
       var r = [];
--- api/points/index.js
+++ api/points/index.js
@@ -1,13 +1,44 @@
 const express = require('express');
+const fs = require('fs');
 const router = express.Router();
+
+var sqlQuery = fs.readFileSync('queries/amen-test.sql').toString();
 
 
 router.get('/', function(req, res, next) {
   console.log('Accessing /api/points');
-	res.json({
-		message: 'Accessing /api/points route',
-		status: 'OK'
-	});
+  console.log(sqlQuery);
+  const pool = req.app.get('pool');
+
+  pool.connect((err, client, done) => {
+    if (err) throw err;
+    client.query(sqlQuery, ['guest_house'], (err, result) => {
+      done();
+      console.log('Returned ' + result.rowCount + ' rows');
+
+      var lat = req.query.lat;
+      var lon = req.query.lon;
+      var i, j;
+      for (j in req.query) {
+        if (Array.isArray(req.query[j])) {
+          for (i in req.query[j]) {
+            console.log(j + ' ' + req.query[j][i]);
+          }
+        }
+        else {
+          console.log(j + ' ' + req.query[j]);
+        }
+      }
+
+      var i;
+      var r = [];
+      for (i in result.rows) {
+        r.push(result.rows[i]);
+      }
+
+      res.json(r);
+    });
+  });
 });
 
 
