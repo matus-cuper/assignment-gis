cfe365860ea68f04084a6194a311ce8d85f06635
Matus Cuper <matus.cuper@gmail.com>
2017-11-20 18:31:02 +0100

    Add category field to query

--- queries/points.sql
+++ queries/points.sql
@@ -1,4 +1,4 @@
-SELECT ST_DISTANCE(point, ST_TRANSFORM(ST_GEOMFROMEWKT('SRID=4326;POINT(' || $1 || ' ' || $2 || ')'), 3857)) AS distance, name, amenity,
+SELECT ST_DISTANCE(point, ST_TRANSFORM(ST_GEOMFROMEWKT('SRID=4326;POINT(' || $1 || ' ' || $2 || ')'), 3857)) AS distance, name, category, amenity,
 ST_Y(ST_TRANSFORM(point, 4326)) AS lat, ST_X(ST_TRANSFORM(point, 4326)) AS lon
 FROM amenity_point
 WHERE category IN ($3, $4, $5)
